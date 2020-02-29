2837b8501cb267f82696d613e5bc1b592b76a306
Zeeshaan <zeeshaan@touchfoundry.co.za>
2020-02-29 11:52:40 +0200

    creating initial scaffold of the app

---
+++ src/components/SidePanel/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class SidePanel extends Component {
+    render() {
+        return (
+            <div style={{ paddingTop: 20 }}>Side Panel</div>
+        );
+    }
+}
+
+export default SidePanel;
