54e0907912eba6fe6604291be1a87846d0157569
Zeeshaan <zeeshaan@touchfoundry.co.za>
2020-02-29 10:39:45 +0200

    start setting up redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
 
 import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import App from './components/App';
 import Login from './components/Auth/Login';
@@ -10,11 +13,15 @@ import Register from './components/Auth/Register';
 import * as serviceWorker from './serviceWorker';
 import firebase from './firebase';
 
+
+const store = createStore(() => {}, composeWithDevTools());
+
 class Root extends Component {
 
     componentDidMount() {
         firebase.auth().onAuthStateChanged(user => {
             if (user) {
+                console.log('user: ', user);
                 this.props.history.push('/');
             }
         })
@@ -34,9 +41,11 @@ class Root extends Component {
 const RoothWithAuth = withRouter(Root);
 
 ReactDOM.render(
-    <Router>
-        <RoothWithAuth />
-    </Router>,
+    <Provider store={store}>
+        <Router>
+            <RoothWithAuth />
+        </Router>
+    </Provider>,
 document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
