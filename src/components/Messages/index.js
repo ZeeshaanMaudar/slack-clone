0dc66db116c131155648efe212421321673ed549
Zeeshaan <zeeshaan@touchfoundry.co.za>
2020-03-01 16:50:02 +0200

    initiate firebase messages db from Messages Component and pass down as props

--- src/components/Messages/index.js
+++ src/components/Messages/index.js
@@ -1,18 +1,31 @@
 import React, { Component, Fragment } from 'react';
 import { Segment, Comment } from 'semantic-ui-react';
+import firebase from '../../firebase';
 
 import MessagesHeader from './MessagesHeader';
 import MessageForm from './MessageForm';
 
 class Messages extends Component {
+    state ={
+        messagesRef: firebase.database().ref('messages'),
+        channel: this.props.currentChannel,
+        user: this.props.currentUser
+    }
+
     render() {
+        const { messagesRef, channel, user } = this.state;
+
         return (
             <Fragment>
                 <MessagesHeader />
                 <Segment>
                     <Comment.Group className='messages'></Comment.Group>
                 </Segment>
-                <MessageForm />
+                <MessageForm
+                    {...{ messagesRef }}
+                    currentUser={user}
+                    currentChannel={channel}
+                />
             </Fragment>
         );
     }
