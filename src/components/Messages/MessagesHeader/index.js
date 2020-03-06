d910013be9a8b5509b2da05fd201f8ae0dff09eb
Zeeshaan <zeeshaan@touchfoundry.co.za>
2020-03-06 08:00:39 +0200

    pass number of unique users as props

--- src/components/Messages/MessagesHeader/index.js
+++ src/components/Messages/MessagesHeader/index.js
@@ -3,7 +3,7 @@ import { Header, Segment, Input, Icon } from 'semantic-ui-react';
 
 export class MessagesHeader extends Component {
     render() {
-        const { channelName } = this.props;
+        const { channelName, numUniqueUsers } = this.props;
 
         return (
             <Segment clearing>
@@ -17,7 +17,7 @@ export class MessagesHeader extends Component {
                         {channelName}
                         <Icon name='star outline' color='black' />
                     </span>
-                    <Header.Subheader>2 Users</Header.Subheader>
+                    <Header.Subheader>{numUniqueUsers}</Header.Subheader>
                 </Header>
                 <Header floated='right'>
                     <Input
