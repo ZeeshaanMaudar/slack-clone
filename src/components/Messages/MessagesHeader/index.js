c102583acc5dbd9d739226da037452c5b14eeb05
Zeeshaan <zeeshaan@touchfoundry.co.za>
2020-03-07 18:58:18 +0200

    add condition if isPrivateChannel

--- src/components/Messages/MessagesHeader/index.js
+++ src/components/Messages/MessagesHeader/index.js
@@ -3,7 +3,7 @@ import { Header, Segment, Input, Icon } from 'semantic-ui-react';
 
 export class MessagesHeader extends Component {
     render() {
-        const { channelName, numUniqueUsers, handleSearchChange, searchLoading } = this.props;
+        const { channelName, numUniqueUsers, handleSearchChange, searchLoading, isPrivateChannel } = this.props;
 
         return (
             <Segment clearing>
@@ -15,7 +15,7 @@ export class MessagesHeader extends Component {
                 >
                     <span>
                         {channelName}
-                        <Icon name='star outline' color='black' />
+                        {!isPrivateChannel && <Icon name='star outline' color='black' />}
                     </span>
                     <Header.Subheader>{numUniqueUsers}</Header.Subheader>
                 </Header>
